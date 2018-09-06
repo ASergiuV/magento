028e411abf0f1da60002abfa367bc4d4270f4cea
Abrudean Sergiu <sergiu.abrudean@evozon.com>
2018-09-06 16:16:50 +0300

    Add Question block

---
+++ app/code/local/Evozon/Questions/Block/Question.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author   Abrudean Sergiu <sergiu.abrudean@evozon.com>
+ * @category Evozon
+ * @package  Evozon_Questions
+ *
+ */
+class Evozon_Modul_Block_Question extends Mage_Core_Block_Template
+{
+    /**
+     * @return string
+     * @author   Abrudean Sergiu <sergiu.abrudean@evozon.com>
+     */
+    public function getQuestion()
+    {
+        return "";
+    }
+}
